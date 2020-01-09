277d566ea9373bfa58d1d7a1befd6dcc08eb1b37
Daniel-Fermoselle <daniel.fermoselle@gmail.com>
2020-01-09 16:00:59 +0000

    Updated Readme. Added config and juno_startup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Script-It-Julia
+To work properly you will need to copy the config folder to your .julia folder.
---
+++ config/juno_startup.jl
@@ -0,0 +1,7 @@
+import ScriptIt
+
+using Khepri
+backend(autocad)
+traceability(true)
+excluded_modules([Base, Khepri, Atom, Atom.CodeTools, Base.CoreLogging,ScriptIt])
+ScriptIt.startScript()
